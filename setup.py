0ad62a1288fbf1b9494b382d80355e2a6a8137df
Drew Perttula <drewp@bigasterisk.com>
2009-07-29 00:12:40 -0700

    version to 1.1
    Ignore-this: 6be000d5043717475668850b09dafc41
    
    darcs-hash:20090729071240-836b1-138c52642a589c2d2add833b8969160bb44bba26.gz

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
 
 setup(name="xtest",
+      version="1.1",
       ext_modules=[
         Extension("xtest",
                   ["xtest.pyx"],

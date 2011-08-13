8291a9a0bf877eb99c8705e849a4c120f4e5c5fa
Drew Perttula <drewp@bigasterisk.com>
2011-08-12 21:34:03 -0700

    move metadata into setup.py
    Ignore-this: c0a7ad83cf858754bc7782919c6c02ec
    
    darcs-hash:20110813043403-836b1-24d655107aca7ce26464d18d995ce3d09e4dd0ee.gz

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
 
 setup(name="xtest",
-      version="1.1",
+      version="1.21",
+      description="pyrex interface to XTest including faking multi-key presses such as Control-Shift-s (version 1.21 adds fakeRelativeMotionEvent and the missing flush calls)",
+      author="Drew Perttula",
+      author_email="drewp@bigasterisk.com",
+      url="http://bigasterisk.com/darcs/?r=xtest",
+      download_url="http://projects.bigasterisk.com/xtest-1.2.tar.gz",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: X11 Applications",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       ext_modules=[
         Extension("xtest",
                   ["xtest.pyx"],
